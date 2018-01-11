f229f08c5a5b99e69b98255c4098081088376e81
Javier Buitrago <jbuitrago2014@fau.edu>
2018-01-10 21:42:37 -0500

    Added some structure to the project

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,9 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { CareerComponent } from "./careers/career.component";
+import { CareerListComponent } from "./careers/career-list.component";
+
+
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [
+      AppComponent,
+      HomeComponent,
+      CareerComponent,
+      CareerListComponent
+
+  ],
+
     imports: [BrowserModule],
     bootstrap: [AppComponent]
 })
---
+++ assets/app/careers/career-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component ({
+selector: 'app-career-list',
+templateUrl: './career-list.component.html',
+styleUrls: ['./career-list.component.css'],
+
+
+})
+
+export class CareerListComponent {
+
+
+
+
+
+
+    }
